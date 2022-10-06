db01d01206f1438e0a9a8f9d9ebff0d026f563a6
Jovana Kisin <if21b095@technikum-wien.at>
2022-10-06 17:36:16 +0200

    fist version

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        User player = new User("Player", "player1");
+        System.out.println(player.username);
+        System.out.println(player.password);
+
+    }
+
+}
---
+++ src/User.java
@@ -0,0 +1,10 @@
+public class User {
+    String username;
+    String password;
+    int coins;
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+}
